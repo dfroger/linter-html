6810da92bf918edc5885ebd76f0089c142c5a8b4
David Froger <david.froger@mailoo.org>
2017-12-03 22:18:52 +0100

    setup server files

---
+++ back/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'linter-html-server',
+    version = '0.0.1',
+    description = 'navigate in swarm of linter warnings',
+    url = 'https://github.com/dfroger/linter-html',
+    entry_points = {
+        'console_scripts': [
+            'linter-html-server = server:main',
+        ],
+    },
+    license = 'GPL V3',
+    author = 'David Froger',
+    author_email = 'david.froger@mailoo.org',
+    requires = ['aiohttp']
+)
